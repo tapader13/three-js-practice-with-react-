95bb7698cc9b887cc04cf3cf619cd060f1d56b6c
tapader13 <minhajtapader0@gmail.com>
2024-07-29 04:27:04 -0400

    model practice

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import {
   MeshDistortMaterial,
   MeshWobbleMaterial,
   OrbitControls,
+  RenderTexture,
+  Text,
   Text3D,
   useHelper,
+  PerspectiveCamera,
+  Sphere,
 } from '@react-three/drei';
 import { DirectionalLightHelper } from 'three';
 // const RotatingBox = () => {
@@ -28,7 +32,29 @@ import { DirectionalLightHelper } from 'three';
 //     </>
 //   );
 // };
+// const RotatingBoxWithText = () => {
+//   const ref = useRef();
+//   useFrame((state, delta) => {
+//     ref.current.position.x = Math.sin(state.clock.elapsedTime);
+//   });
 
+//   return (
+//     <>
+//       <mesh>
+//         <boxGeometry args={[2, 2, 2]} />
+//         <meshStandardMaterial>
+//           <RenderTexture attach={'map'}>
+//             <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+//             <color attach={'background'} args={['pink']} />
+//             <Text fontSize={1.5} color={'black'} ref={ref}>
+//               hello
+//             </Text>
+//           </RenderTexture>
+//         </meshStandardMaterial>
+//       </mesh>
+//     </>
+//   );
+// };
 // const SphereGeometry = () => {
 //   const ref = useRef();
 //   const [isHover, setIsHover] = useState(false);
@@ -58,33 +84,47 @@ import { DirectionalLightHelper } from 'three';
 //   );
 // };
 
-const TorusKnotGeometry = () => {
-  const directionalLightRef = useRef();
-  useHelper(directionalLightRef, DirectionalLightHelper, 0.7, 'white');
-  return (
-    <>
-      <directionalLight position={[0, 1, 2]} ref={directionalLightRef} />
+// const TorusKnotGeometry = () => {
+//   const directionalLightRef = useRef();
+//   useHelper(directionalLightRef, DirectionalLightHelper, 0.7, 'white');
+//   return (
+//     <>
+//       <directionalLight position={[0, 1, 2]} ref={directionalLightRef} />
 
-      <mesh position={[0, -1, 0]}>
-        <torusKnotGeometry args={[2, 0.2, 16, 50]} />
-        <meshStandardMaterial color='red' />
-        {/* <MeshWobbleMaterial factor={5} speed={1} /> */}
-        {/* <MeshDistortMaterial color={'red'} distort={1} speed={1} /> */}
-      </mesh>
-    </>
-  );
-};
+//       <mesh position={[0, -1, 0]}>
+//         <torusKnotGeometry args={[2, 0.2, 16, 50]} />
+//         <meshStandardMaterial color='red' />
+//         {/* <MeshWobbleMaterial factor={5} speed={1} /> */}
+//         {/* <MeshDistortMaterial color={'red'} distort={1} speed={1} /> */}
+//       </mesh>
+//     </>
+//   );
+// };
 
 const App = () => {
   return (
-    <div>
+    <div style={{ position: 'relative', width: '600px' }}>
+      {/* <Canvas camera={{ fov: 25, position: [5, 5, 5] }}> */}
       <Canvas>
-        <ambientLight />
-        <directionalLight position={[10, 10, 10]} />
-        {/* <RotatingBox /> */}
+        <ambientLight intensity={1} />
+        <directionalLight position={[3, 2, 1]} />
+        {/* <RotatingBox />    */}
+        {/* <RotatingBoxWithText /> */}
         {/* <SphereGeometry /> */}
-        <TorusKnotGeometry />
-        <OrbitControls />
+        {/* <TorusKnotGeometry /> */}
+        <OrbitControls autoRotate enableZoom={false} />
+
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={1} />
+        <directionalLight position={[3, 2, 1]} />
+        <Sphere args={[1, 220, 350]} scale={3}>
+          <MeshDistortMaterial
+            color='#3d1c56'
+            attach='material'
+            distort={0.5}
+            speed={2}
+          />
+        </Sphere>
 
         {/* <group position={[0, 1, 0]}> */}
         {/* Box Geometry */}
@@ -124,6 +164,17 @@ const App = () => {
           <meshStandardMaterial color='orange' />
         </mesh> */}
       </Canvas>
+      <img
+        style={{
+          height: '400px',
+          width: '400px',
+          position: 'absolute',
+          left: '50px',
+          top: '0',
+        }}
+        src='../moon.png'
+        alt=''
+      />
     </div>
   );
 };
